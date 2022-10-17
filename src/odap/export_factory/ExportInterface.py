d43fe1fcd1026acb8b34b6b760323e7bdeb2a439
Lukáš Langr <82815273+ds-lukaslangr@users.noreply.github.com>
2022-10-17 10:09:20 +0200

    :coffin: Remove unused code

--- src/odap/export_factory/ExportInterface.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Dict, Optional
-
-
-class ExportInterface(ABC):
-    @abstractmethod
-    def export(self, config: Optional[Dict] = None):
-        pass
