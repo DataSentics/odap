a7194ee372d8c96be8e0b977165ea07bd7defbe0
KubaVaclavik <88331896+KubaVaclavik@users.noreply.github.com>
2022-10-27 13:33:11 +0200

    :sparkles: Enhanced segment export (#8)

--- src/odap/segment_factory/segments.py
+++ src/odap/segment_factory/segments.py
@@ -1,9 +1,7 @@
-from typing import Dict, Any
 from pyspark.sql import DataFrame
 from odap.common.databricks_context import get_workspace_api, resolve_dbutils
 from odap.common.dataframes import create_dataframe_from_notebook_cells
 from odap.common.utils import get_absolute_path, get_notebook_cells
-from odap.segment_factory.config import get_segment_table_path, get_segment_table
 from odap.segment_factory.exceptions import SegmentNotFoundException
 
 
@@ -19,12 +17,3 @@ def create_segment_dataframe_by_slug(slug: str) -> DataFrame:
     if not segment_df:
         raise SegmentNotFoundException(f"Segment '{slug}' could not be loaded")
     return segment_df
-
-
-def save_segment_dataframe(df: DataFrame, slug: str, config: Dict[str, Any]) -> None:
-    segment_db_path = get_segment_table_path(slug, config)
-    segment_db_name = get_segment_table(slug, config)
-
-    df.write.mode("overwrite").option("overwriteSchema", True).option("path", segment_db_path).saveAsTable(
-        segment_db_name
-    )
