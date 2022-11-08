c77da50473a1da087f0a47e30125963dd486b5b6
Jakub Vaclavik <jakub.vaclavik@datasentics.com>
2022-11-08 17:06:53 +0100

    :sparkles: Export segments union

--- src/odap/segment_factory/exports.py
+++ src/odap/segment_factory/exports.py
@@ -7,10 +7,11 @@ from odap.feature_factory.config import (
 )
 from odap.common.config import TIMESTAMP_COLUMN
 from odap.feature_factory.metadata_schema import LAST_COMPUTE_DATE
-from odap.segment_factory.config import get_export, get_segment
+from odap.segment_factory.config import get_destination, get_export
 from odap.segment_factory.exporters import resolve_exporter
 from odap.segment_factory.logs import write_export_log
-from odap.segment_factory.segments import create_segment_df
+from odap.segment_factory.schemas import SEGMENT
+from odap.segment_factory.segments import create_segments_union_df
 from odap.common.logger import logger
 
 
@@ -28,8 +29,8 @@ def create_leatest_features_df(entity_name: str, feature_factory_config: Dict):
     )
 
 
-def join_segment_with_entities(segment_df: DataFrame, export_config: Any, feature_factory_config: Dict):
-    for entity_name in export_config.get("attributes").keys():
+def join_segment_with_entities(segment_df: DataFrame, destination_config: Any, feature_factory_config: Dict):
+    for entity_name in destination_config.get("attributes").keys():
         id_column = get_entity_by_name(entity_name, feature_factory_config).get("id_column")
 
         leatest_features_df = create_leatest_features_df(entity_name, feature_factory_config)
@@ -41,24 +42,29 @@ def join_segment_with_entities(segment_df: DataFrame, export_config: Any, featur
     return segment_df
 
 
-def select_attributes(df: DataFrame, export_config: Any):
-    select_columns = [attribute for attributes in export_config.get("attributes").values() for attribute in attributes]
-    return df.select(*select_columns)
+def select_attributes(df: DataFrame, destination_config: Any):
+    select_columns = [
+        attribute for attributes in destination_config.get("attributes").values() for attribute in attributes
+    ]
+    return df.select(SEGMENT, *select_columns)
 
 
 # pylint: disable=too-many-statements
-def run_export(segment_name: str, export_name: str, feature_factory_config: Dict, segment_factory_config: Dict):
-    logger.info(f"Exporting '{segment_name}' segment to '{export_name}'...")
+def run_export(export_name: str, feature_factory_config: Dict, segment_factory_config: Dict):
+    logger.info(f"Running export '{export_name}'...")
 
-    segment_config = get_segment(segment_name, segment_factory_config)
     export_config = get_export(export_name, segment_factory_config)
+    destination_config = get_destination(export_config["destination"], segment_factory_config)
 
-    segment_df = create_segment_df(segment_name)
-    joined_segment_featurestores_df = join_segment_with_entities(segment_df, export_config, feature_factory_config)
-    final_export_df = select_attributes(joined_segment_featurestores_df, export_config)
+    united_segments_df = create_segments_union_df(export_config["segments"])
+    joined_segment_featurestores_df = join_segment_with_entities(
+        united_segments_df, destination_config, feature_factory_config
+    )
+    final_export_df = select_attributes(joined_segment_featurestores_df, destination_config)
 
-    exporter_fce = resolve_exporter(export_config["type"])
-    exporter_fce(segment_name, final_export_df, segment_config, export_config)
+    exporter_fce = resolve_exporter(destination_config["type"])
+    exporter_fce(export_name, final_export_df, export_config, destination_config)
 
-    write_export_log(segment_df, segment_name, export_name, segment_factory_config)
+    write_export_log(united_segments_df, export_name, segment_factory_config)
+
     logger.info("Export successful.")
--- src/odap/segment_factory/segments.py
+++ src/odap/segment_factory/segments.py
@@ -1,27 +1,22 @@
-from datetime import datetime
-from typing import Dict
-from pyspark.sql import DataFrame
+from functools import reduce
+from typing import Any, Dict
+from pyspark.sql import DataFrame, SparkSession, types as t, functions as f
 from odap.common.logger import logger
 from odap.common.databricks import get_workspace_api
 from odap.common.dataframes import create_dataframe, create_dataframe_from_notebook_cells
 from odap.segment_factory.config import get_segment_table, get_segment_table_path
 from odap.common.utils import get_absolute_path, get_notebook_cells, get_notebook_language
 from odap.segment_factory.exceptions import SegmentNotFoundException
-from odap.segment_factory.schemas import get_segment_common_fields_schema
+from odap.segment_factory.schemas import SEGMENT, get_segment_common_fields_schema
 
 
 def write_segment(
     df: DataFrame,
-    segment_name: str,
     export_id: str,
-    export_name: str,
-    timestamp: datetime,
     segment_factory_config: Dict,
 ):
 
-    extended_segment_df = create_dataframe(
-        [[export_id, timestamp, segment_name, export_name]], get_segment_common_fields_schema()
-    ).join(df, how="full")
+    extended_segment_df = create_dataframe([[export_id]], get_segment_common_fields_schema()).join(df, how="full")
 
     table = get_segment_table(segment_factory_config)
     logger.info(f"Writing segment to table: '{table}'")
@@ -29,9 +24,9 @@ def write_segment(
         extended_segment_df.write.format("delta")
         .mode("append")
         .option("path", get_segment_table_path(segment_factory_config))
+        .option("mergeSchema", "True")
         .saveAsTable(table)
     )
-    return table
 
 
 def create_segment_df(segment_name: str) -> DataFrame:
@@ -46,3 +41,15 @@ def create_segment_df(segment_name: str) -> DataFrame:
     if not segment_df:
         raise SegmentNotFoundException(f"Segment '{segment_name}' could not be loaded")
     return segment_df
+
+
+def union_segments(prev_df: DataFrame, segment_name: str):
+    segment_df = create_segment_df(segment_name)
+    segment_df = segment_df.withColumn(SEGMENT, f.lit(segment_name)).select("segment", *segment_df.columns)
+    return prev_df.unionByName(segment_df, allowMissingColumns=True)
+
+
+def create_segments_union_df(segments_config: Dict[str, Any]) -> DataFrame:
+    spark = SparkSession.getActiveSession()
+    empty_df = spark.createDataFrame([], t.StructType([]))
+    return reduce(union_segments, segments_config.keys(), empty_df)
