7cad13cba8d3f19fd568dc9791e1c315bf4451fc
Karel Gudera <karel.gudera@gmail.com>
2022-11-25 11:21:27 +0100

    :bug: Fixed no target optimizer

--- src/odap/feature_factory/no_target_optimizer.py
+++ src/odap/feature_factory/no_target_optimizer.py
@@ -2,8 +2,11 @@ from typing import List
 
 import re
 
+from pyspark.sql import SparkSession
+
 from odap.feature_factory import const
 from odap.common.widgets import get_widget_value
+from odap.common.config import TIMESTAMP_COLUMN
 
 
 def replace_sql_target_join(cell: str) -> str:
@@ -13,13 +16,13 @@ def replace_sql_target_join(cell: str) -> str:
     if found_join and found_select_start:
         cell = cell.replace(found_join.group(0), "")
         start = found_select_start.start(1)
-        cell = cell[:start] + const.SQL_TIMESTAMP_LIT + cell[start:]
+        cell = cell[:start] + const.SQL_TIMESTAMP_LIT.format(timestamp=get_timestamp()) + cell[start:]
 
     return cell
 
 
 def replace_pyspark_target_join(cell: str) -> str:
-    return re.sub(const.PYTHON_TARGET_STORE_JOIN_REGEX, const.PYTHON_TIMESTAMP_LIT, cell)
+    return re.sub(const.PYTHON_TARGET_STORE_JOIN_REGEX, const.PYTHON_TIMESTAMP_LIT.format(timestamp=get_timestamp()), cell)
 
 
 def replace_no_target(notebook_language: str, notebook_cells: List[str]):
@@ -31,3 +34,9 @@ def replace_no_target(notebook_language: str, notebook_cells: List[str]):
             notebook_cells[idx] = replace_sql_target_join(cell)
         elif notebook_language == "PYTHON":
             notebook_cells[idx] = replace_pyspark_target_join(cell)
+
+
+def get_timestamp():
+    spark = SparkSession.getActiveSession()
+
+    return spark.read.table(const.TARGET_STORE).limit(1).select(TIMESTAMP_COLUMN).collect()[0][0]
