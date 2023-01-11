5996e794b2ccf94c0088c4dcda9286311c1c590a
Karel Gudera <karel.gudera@gmail.com>
2023-01-11 11:25:49 +0100

    Update feature_notebook.py

--- odap-package/src/odap/feature_factory/feature_notebook.py
+++ odap-package/src/odap/feature_factory/feature_notebook.py
@@ -40,7 +40,7 @@ class FeatureNotebook:
         info = notebook_info
         cells = get_feature_notebook_cells(notebook_info, workspace_api)
         df = create_dataframe_from_notebook_cells(info, cells[:])
-        metadata = resolve_metadata(cells, info.path, df)
+        metadata = ""
         df_check_list = get_dq_checks_list(info, cells)
 
         return cls(info, df, metadata, config, df_check_list)
