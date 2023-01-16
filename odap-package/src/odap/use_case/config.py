0f797f9e3cf098585d014504664b44c5e97f1b77
Lukáš Langr <82815273+ds-lukaslangr@users.noreply.github.com>
2023-01-16 13:44:29 +0100

    :art: Final fixes

--- odap-package/src/odap/use_case/config.py
+++ odap-package/src/odap/use_case/config.py
@@ -1,5 +1,15 @@
-from odap.common.config import get_config_on_rel_path, CONFIG_NAME
+from odap.common.config import get_config_namespace, get_config_on_rel_path, CONFIG_NAME, ConfigNamespace
+from odap.common.exceptions import ConfigAttributeMissingException
 from odap.segment_factory.config import USE_CASES_FOLDER
+
+
+def get_use_case_table():
+    table = get_config_namespace(ConfigNamespace.USECASE_FACTORY).get("table")
+
+    if not table:
+        raise ConfigAttributeMissingException("usecasefactory.table not defined in config.yaml")
+
+    return table
 
 
 def get_use_case_config(use_case: str) -> dict:
