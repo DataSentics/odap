c77a303e9b0920496c5f8e1ba3ac40e641b19fde
Karel Gudera <karel.gudera@gmail.com>
2023-01-26 15:09:18 +0100

    :bug: Fixed hive_table_exists function

--- odap-package/src/odap/common/tables.py
+++ odap-package/src/odap/common/tables.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import StructType
 from delta import DeltaTable
@@ -9,16 +9,29 @@ from odap.common.logger import logger
 
 def hive_table_exists(full_table_name: str) -> bool:
     spark = SparkSession.getActiveSession()
+    catalog, database, table = full_table_name.split(".")
 
-    db_name = full_table_name.split(".")[0]
-    table_name = full_table_name.split(".")[1]
-    databases = [db.databaseName for db in spark.sql("SHOW DATABASES").collect()]
+    if catalog not in get_catalogs():
+        return False
 
-    if db_name not in databases:
+    if database not in get_databases(catalog):
         return False
 
     # pylint: disable=use-implicit-booleaness-not-comparison
-    return spark.sql(f'SHOW TABLES IN {db_name} LIKE "{table_name}"').collect() != []
+    return spark.sql(f'SHOW TABLES IN {database} LIKE "{table}"').collect() != []
+
+
+def get_catalogs() -> List[str]:
+    spark = SparkSession.getActiveSession()
+    return [
+        "hive_metastore" if row.catalog == "spark_catalog" else row.catalog
+        for row in spark.sql("SHOW CATALOGS").collect()
+    ]
+
+
+def get_databases(catalog: str) -> List[str]:
+    spark = SparkSession.getActiveSession()
+    return [row.databaseName for row in spark.sql(f"SHOW DATABASES IN {catalog}").collect()]
 
 
 def feature_store_table_exists(full_table_name: str) -> bool:
