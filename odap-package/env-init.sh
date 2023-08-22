ab713296bd72dc7dfcd1634799c4110f08e2e960
Lukáš Langr <lukas.langr@datasentics.com>
2023-08-22 17:26:11 +0200

    :green_heart: Fix env init

--- odap-package/env-init.sh
+++ odap-package/env-init.sh
@@ -1,45 +1,14 @@
-#!/bin/bash -e
+#!/bin/bash
 
-PENVY_VERSION="1.2.5"
-BENVY_VERSION="1.4.5"
-POSSIBLE_PATHS_LIST_URL="https://raw.githubusercontent.com/pyfony/penvy/master/src/penvy/conda/conda_executable_paths.txt"
+# Activate conda
+eval "$(conda shell.bash hook)"
 
-resolve_conda_executable_path() {
-  if hash conda 2>/dev/null; then
-    echo "Using conda from PATH"
+# Create new conda environment and activate it
+conda env create -f environment.yml -p .venv
+conda activate $PWD/.venv
 
-    CONDA_EXECUTABLE_PATH="conda"
-  else
-    POSSIBLE_PATHS_LIST=$(curl --silent $POSSIBLE_PATHS_LIST_URL)
+# Install all dependencies
+poetry install
 
-    while IFS= read -r line; do
-      FILE_PATH=$(sed -E "s|\~|$HOME|g" <<< "$line")
-
-      if [ -f "$FILE_PATH" ]; then
-        echo "Using conda from: $FILE_PATH"
-        CONDA_EXECUTABLE_PATH="$FILE_PATH"
-        break
-      fi
-    done <<< "$POSSIBLE_PATHS_LIST"
-
-    if [ -z ${CONDA_EXECUTABLE_PATH+x} ]; then
-      echo "Unable to resolve Conda executable path"
-      exit 1
-    fi
-  fi
-}
-
-resolve_conda_executable_path
-
-CONDA_BASE_DIR=$($CONDA_EXECUTABLE_PATH info --base | sed 's/\\/\//g')
-
-if [ -d "$CONDA_BASE_DIR/Scripts" ]; then
-  PYTHON_EXECUTABLE="$CONDA_BASE_DIR/python.exe" # Windows
-else
-  PYTHON_EXECUTABLE="$CONDA_BASE_DIR/bin/python" # Linux/Mac
-fi
-
-SITE_PACKAGES_PATH=$($PYTHON_EXECUTABLE -c 'import sysconfig; print(sysconfig.get_paths()["purelib"])')
-
-$PYTHON_EXECUTABLE -m pip install "penvy==$PENVY_VERSION" "benvy==$BENVY_VERSION"
-$PYTHON_EXECUTABLE "$SITE_PACKAGES_PATH/benvy/init.py" "$@"
+# Copy dotenv
+cp .env.dist .env
