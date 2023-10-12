c0f736f7550a4ed35683ae07edfc4ed4124a5476
Lukáš Langr <lukas.langr@datasentics.com>
2023-10-12 12:58:04 +0200

    :bug: Fix widgets

--- odap-package/src/odap/feature_factory/widgets.py
+++ odap-package/src/odap/feature_factory/widgets.py
@@ -2,7 +2,7 @@ from odap.common.config import get_config_namespace, ConfigNamespace
 from odap.common.databricks import resolve_dbutils, get_workspace_api
 from odap.common.utils import get_notebook_name
 from odap.feature_factory import const
-from odap.feature_factory.config import get_feature_sources
+from odap.feature_factory.config import get_feature_sources, get_feature_source_dir, get_feature_source_prefix
 from odap.feature_factory.feature_notebooks_selection import get_feature_notebooks_info
 
 
@@ -10,17 +10,20 @@ def create_notebooks_widget():
     dbutils = resolve_dbutils()
 
     config = get_config_namespace(ConfigNamespace.FEATURE_FACTORY)
-    feature_dir = get_feature_sources(config)
+    feature_sources = get_feature_sources(config)
+    feature_notebooks_all = []
 
-    for repo in feature_dir:
-        feature_dir = repo.get("path", "")
+    for feature_source in feature_sources:
+        features_dir = get_feature_source_dir(feature_source)
+        prefix = get_feature_source_prefix(feature_source)
 
-        features = [
-            get_notebook_name(notebook_info.path)
-            for notebook_info in get_feature_notebooks_info(get_workspace_api(), feature_dir)
+        feature_notebooks = [
+            f"[{prefix}] {get_notebook_name(notebook_info.path)}"
+            for notebook_info in get_feature_notebooks_info(get_workspace_api(), features_dir)
         ]
+        feature_notebooks_all.extend(feature_notebooks)
 
-    dbutils.widgets.multiselect(const.FEATURE_WIDGET, const.ALL_FEATURES, [const.ALL_FEATURES] + features)
+    dbutils.widgets.multiselect(const.FEATURE_WIDGET, const.ALL_FEATURES, [const.ALL_FEATURES] + feature_notebooks)
 
 
 def create_dry_run_widgets():
